d7d20af725497b2581675db8060839949aaefe15
Andy Gray <andy@whatsthescore.co.uk>
2018-03-05 14:53:11 +0000

    new files

---
+++ src/components/Account.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="account">
+
+    <h1>{{ account }}</h1>
+
+    <p>My balance: </p>
+
+  </div>
+</template>
+
+<script>
+
+  import {mapGetters, mapState} from 'vuex'
+
+  export default {
+    name: 'dashboard',
+    components: {},
+    data() {
+      return {
+        title: 'Hello',
+      }
+    },
+    computed: {
+      ...mapState([
+        'account'
+      ])
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Token.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="token">
+
+    <h1>Token name: {{ contractName }}</h1>
+
+    <h2>Symbol: {{ contractSymbol }}</h2>
+
+  </div>
+</template>
+
+<script>
+
+  import {mapGetters, mapState} from 'vuex'
+
+  export default {
+    name: 'dashboard',
+    components: {},
+    data() {
+      return {
+        title: 'Hello',
+      }
+    },
+    computed: {
+      ...mapState([
+        'contractName',
+        'contractSymbol',
+      ])
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
