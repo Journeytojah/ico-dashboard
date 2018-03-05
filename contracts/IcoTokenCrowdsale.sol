eda72e20c15b47fbe1f9a5806fb9c64492d37252
Andy Gray <andy@whatsthescore.co.uk>
2018-03-05 16:19:04 +0000

    better sample contract

--- contracts/IcoTokenCrowdsale.sol
+++ contracts/IcoTokenCrowdsale.sol
@@ -1,14 +1,22 @@
 pragma solidity ^0.4.19;
 
 
-import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "zeppelin-solidity/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "zeppelin-solidity/contracts/crowdsale/distribution/RefundableCrowdsale.sol";
+import "zeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
 
 
-contract IcoTokenCrowdsale is CappedCrowdsale {
+contract IcoTokenCrowdsale is CappedCrowdsale, RefundableCrowdsale, MintedCrowdsale {
 
-  function IcoTokenCrowdsale(uint256 _rate, address _wallet, ERC20 _token, uint256 _cap) public
+  function IcoTokenCrowdsale(uint256 _openingTime, uint256 _closingTime, uint256 _rate, address _wallet, uint256 _cap, MintableToken _token, uint256 _goal) public
   Crowdsale(_rate, _wallet, _token)
-  CappedCrowdsale(_cap) {}
-
+  CappedCrowdsale(_cap)
+  TimedCrowdsale(_openingTime, _closingTime)
+  RefundableCrowdsale(_goal)
+  {
+    //As goal needs to be met for a successful crowdsale
+    //the value needs to less or equal than a cap which is limit for accepted funds
+    require(_goal <= _cap);
+  }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,27 @@
+/* global web3:true */
+
 const IcoToken = artifacts.require("IcoToken");
 const IcoTokenCrowdsale = artifacts.require("IcoTokenCrowdsale");
 
-const BigNumber = web3.BigNumber;
-
 module.exports = function (deployer, network, accounts) {
-  deployer.deploy(IcoToken).then(function() {
-    return deployer.deploy(IcoTokenCrowdsale, new BigNumber(1), accounts[0], IcoToken.address, new BigNumber(1000))
+  deployer.deploy(IcoToken).then(function () {
+
+    const opening = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1;// one second in the future
+    const closing = opening + (86400 * 20); // 20 days
+    const rate = web3.toWei(0.1, 'ether');
+    const cap = web3.toWei(1000000, 'ether');
+    const goal = web3.toWei(33333, 'ether');
+    const wallet = accounts[0];
+
+    return deployer.deploy(
+      IcoTokenCrowdsale,
+      opening,
+      closing,
+      rate,
+      wallet,
+      cap,
+      IcoToken.address,
+      goal
+    )
   });
 };
