fbb58d054ab347e52dc3af1efabc698305b42030
Andy Gray <andy@whatsthescore.co.uk>
2018-03-06 16:22:48 +0000

    Lowered rate

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,9 +6,9 @@ const ABCTokenCrowdsale = artifacts.require("ABCTokenCrowdsale");
 module.exports = function (deployer, network, accounts) {
   deployer.deploy(ABCToken).then(function () {
 
-    const opening = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1;// one second in the future
+    const opening = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1; // one second in the future
     const closing = opening + (86400 * 20); // 20 days
-    const rate = 2000000000000000;
+    const rate = 1; // TODO think about this...
     const cap = web3.toWei(50000, 'ether');
     const goal = web3.toWei(20000, 'ether');
     const wallet = accounts[0];
