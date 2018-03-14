b3d1203f5c934149a0a21f40184c470d75499ee5
James Morgan <james.morgan.e@googlemail.com>
2018-03-14 10:07:18 +0000

    Adding basic solidity code coverage

--- truffle.js
+++ truffle.js
@@ -1,4 +1,7 @@
 module.exports = {
+  mocha: {
+    useColors: true
+  },
   networks: {
     development: {
       host: "127.0.0.1",
@@ -14,6 +17,13 @@ module.exports = {
       host: '127.0.0.1',
       port: 8545,
       network_id: '*'
-    }
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+      gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 0x01      // <-- Use this low gas price
+    },
   }
 };
