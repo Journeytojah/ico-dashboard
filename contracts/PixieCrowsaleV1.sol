c3d4c4d74b2c53d5b87538379198854d75efb5bd
Andy Gray <andy@whatsthescore.co.uk>
2018-03-21 17:02:53 +0000

    flatten crowdsale

---
+++ contracts/PixieCrowsaleV1.sol
@@ -0,0 +1,203 @@
+pragma solidity ^0.4.19;
+
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "zeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
+import "zeppelin-solidity/contracts/lifecycle/Pausable.sol";
+import "zeppelin-solidity/contracts/crowdsale/distribution/utils/RefundVault.sol";
+
+contract PixieCrowdsale is Crowdsale, Pausable {
+
+  event Finalized();
+
+  uint256 public openingTime = now;
+
+  uint256 public closingTime = openingTime.add(4 weeks);
+
+  uint256 public privateSaleCloseTime = openingTime.add(1 weeks);
+
+  uint256 public privateSaleRate = 3;
+
+  uint256 public preSaleCloseTime = openingTime.add(2 weeks);
+
+  uint256 public preSaleRate = 2;
+
+  uint256 public cap = 1000 ether;
+
+  mapping(address => uint256) public contributions;
+
+  uint256 public min = 0.1 ether;
+
+  uint256 public max = 100 ether;
+
+  mapping(address => bool) public whitelist;
+
+  bool public isFinalized = false;
+
+  // minimum amount of funds to be raised in weis
+  uint256 public goal = 50 ether;
+
+  // refund vault used to hold funds while crowdsale is running
+  RefundVault public vault;
+
+  function PixieCrowdsale(address _wallet, StandardToken _token) public Crowdsale(1, _wallet, _token) {
+    vault = new RefundVault(wallet);
+  }
+
+  /**
+   * @dev Investors can claim refunds here if crowdsale is unsuccessful
+   */
+  function claimRefund() public {
+    require(isFinalized);
+    require(!goalReached());
+
+    vault.refund(msg.sender);
+  }
+
+  /**
+   * @dev Checks whether funding goal was reached.
+   * @return Whether funding goal was reached
+   */
+  function goalReached() public view returns (bool) {
+    return weiRaised >= goal;
+  }
+
+  /**
+   * @dev vault finalization task, called when owner calls finalize()
+   */
+  function finalization() internal {
+    if (goalReached()) {
+      vault.close();
+    } else {
+      vault.enableRefunds();
+    }
+  }
+
+  /**
+   * @dev Overrides Crowdsale fund forwarding, sending funds to vault.
+   */
+  function _forwardFunds() internal {
+    vault.deposit.value(msg.value)(msg.sender);
+  }
+
+  /**
+   * @dev Must be called after crowdsale ends, to do some extra finalization
+   * work. Calls the contract's finalization function.
+   */
+  function finalize() onlyOwner public {
+    require(!isFinalized);
+    require(hasClosed());
+
+    finalization();
+    Finalized();
+
+    isFinalized = true;
+  }
+
+  /**
+   * @dev Adds single address to whitelist.
+   * @param _beneficiary Address to be added to the whitelist
+   */
+  function addToWhitelist(address _beneficiary) external onlyOwner {
+    whitelist[_beneficiary] = true;
+  }
+
+  /**
+   * @dev Adds list of addresses to whitelist. Not overloaded due to limitations with truffle testing.
+   * @param _beneficiaries Addresses to be added to the whitelist
+   */
+  function addManyToWhitelist(address[] _beneficiaries) external onlyOwner {
+    for (uint256 i = 0; i < _beneficiaries.length; i++) {
+      whitelist[_beneficiaries[i]] = true;
+    }
+  }
+
+  /**
+   * @dev Removes single address from whitelist.
+   * @param _beneficiary Address to be removed to the whitelist
+   */
+  function removeFromWhitelist(address _beneficiary) external onlyOwner {
+    whitelist[_beneficiary] = false;
+  }
+
+  /**
+   * @dev Extend parent behavior to update user contributions so far
+   * @param _beneficiary Token purchaser
+   * @param _weiAmount Amount of wei contributed
+   */
+  function _updatePurchasingState(address _beneficiary, uint256 _weiAmount) internal {
+    super._updatePurchasingState(_beneficiary, _weiAmount);
+    contributions[_beneficiary] = contributions[_beneficiary].add(_weiAmount);
+  }
+
+  /**
+   * @dev Checks whether the cap has been reached.
+   * @return Whether the cap was reached
+   */
+  function capReached() public view returns (bool) {
+    return weiRaised >= cap;
+  }
+
+  /**
+   * @dev Checks whether the period in which the crowdsale is open has already elapsed.
+   * @return Whether crowdsale period has elapsed
+   */
+  function hasClosed() public view returns (bool) {
+    return now > closingTime;
+  }
+
+  /**
+   * @dev Overridden method used to allow different rates for private/pre sale
+   * @param _weiAmount Value in wei to be converted into tokens
+   * @return Number of tokens that can be purchased with the specified _weiAmount
+   */
+  function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
+    if (now < privateSaleCloseTime) {
+      return _weiAmount.mul(privateSaleRate);
+    }
+
+    if (now < preSaleCloseTime) {
+      return _weiAmount.mul(preSaleRate);
+    }
+
+    return _weiAmount.mul(rate);
+  }
+
+  /**
+   * @dev gets current time
+   * @return the current blocktime
+   */
+  function getNow() public view returns (uint) {
+    return now;
+  }
+
+  /**
+   * @dev Checks whether the period in which the crowdsale is open has elapsed.
+   * @return Whether crowdsale period is open
+   */
+  function isCrowdsaleOpen() public view returns (bool) {
+    return now >= openingTime && now <= closingTime;
+  }
+
+  /**
+  * @dev Extend parent behavior requiring contract to not be paused.
+  * @param _beneficiary Token beneficiary
+  * @param _weiAmount Amount of wei contributed
+  */
+  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
+    super._preValidatePurchase(_beneficiary, _weiAmount);
+
+    require(now >= openingTime && now <= closingTime);
+
+    require(weiRaised.add(_weiAmount) <= cap);
+
+    require(_weiAmount >= min);
+
+    require(contributions[_beneficiary].add(_weiAmount) <= max);
+
+    require(whitelist[_beneficiary]);
+
+    require(!paused);
+  }
+
+
+}
